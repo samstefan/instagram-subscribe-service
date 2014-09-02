12d9c36c5cbae312cff0e07ff18ce76d6231c4c7
Sam Stefan <hello@samstefan.co.uk>
2014-09-02 10:44:49 +0100

    Updated properties with production settings.

--- properties.js
+++ properties.js
@@ -3,7 +3,13 @@ var _ = require('lodash')
 
   , baseProperties =
     { port: 3010
-    , database: {}
+    , database:
+      { user: ''
+      , pass: ''
+      , host: 'localhost'
+      , port: '27017'
+      , name: 'bestival-live'
+      }
     , clientId: 'f9ffe04efa6347c8b97cabafd8896e93'
     , clientSecret: '5bb80ce1fb184b54a168baab4ea8c3ce'
     , hashTags:
@@ -16,17 +22,9 @@ var _ = require('lodash')
     }
 
   , properties =
-    { development:
-      { callbackUrl: 'http://samstefan.x64.me:3010/callback/'
-      , database:
-        { user: ''
-        , pass: ''
-        , host: 'localhost'
-        , port: '27017'
-        , name: 'bestival-live'
-        }
-      }
+    { development: {}
     , testing: {}
+    , production: {}
   }
 
 module.exports = function () {
